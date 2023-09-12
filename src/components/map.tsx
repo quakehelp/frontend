55fec54e137adce887cdcd0c354e52dd8117dace
Naïm Abdelkerim <naimdevelopper@gmail.com>
2023-09-12 23:08:23 +0100

    add map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,5 @@
 
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
 import useMapStore from '../states/map.state';
 
 
@@ -16,14 +16,14 @@ type Props = {
 
 }
 function Map({ className, center, zoom = 8 }: Props) {
-    const {onLoad,onError,onUnmount}=useMapStore()
+    const { onLoad, onError, onUnmount, locations, activeLocation, setActiveLocation } = useMapStore()
     return (
         <LoadScript
             googleMapsApiKey={import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY as string}
             onLoad={onLoad}
             onError={onError}
             onUnmount={onUnmount}
-            
+
         >
             <GoogleMap
                 mapContainerClassName={className}
@@ -31,10 +31,36 @@ function Map({ className, center, zoom = 8 }: Props) {
                 zoom={zoom}
                 options={{
                     disableDefaultUI: true,
-                    
+
                 }}
             >
-        </GoogleMap>
+                {locations?.map(location => (
+                    <Marker
+                        key={location._id}
+                        position={location.position}
+                        title={location.name}
+                        onClick={() => setActiveLocation(location)}
+                    // onMouseOver={() => setActiveLocation(location)}
+                    // onMouseOut={() => setActiveLocation(null)}
+                    />
+                ))}
+                {activeLocation && (
+                    <InfoWindow
+                        position={activeLocation.position}
+                       // onCloseClick={() => setActiveLocation(null)}
+                        
+                    >
+                        <div>
+                            <h4>{activeLocation.locationName}</h4>
+                            <p>Population: {activeLocation.population}</p>
+                            <p>Medical Help Needed: {activeLocation.medicalHelp}</p>
+                            <p>Basic Help Needed: {activeLocation.basicHelp}</p>
+                            <p>Deceased: {activeLocation.deceased}</p>
+                            <p>Needs: {activeLocation.needs.join(", ")}</p>
+                        </div>
+                    </InfoWindow>
+                )}
+            </GoogleMap>
         </LoadScript>
     );
 }
